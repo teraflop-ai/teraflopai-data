b3b4b54e52cea695ac6e0025d48876096f513d5e
Enrico Shippole <enricoship@gmail.com>
2025-09-01 22:53:55 -0400

    Image hashing

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # teraflopai-data
 
-A petabyte scale data processing framework for AI models using Ray.
+A petabyte scale data processing framework for AI models using Daft + Ray.
 
 ## Installation
 ```python
 pip install teraflopai-data
 ```
+
+### Text
+
+### Image
+
+### Video
+
+## Slurm
+
---
+++ src/teraflopai_data/components/image/image_hashing.py
@@ -0,0 +1,58 @@
+import io
+from enum import Enum
+from typing import Optional
+
+import imagehash
+from PIL import Image
+
+
+class HashingAlgorithm(str, Enum):
+    """Available hashing algorithms."""
+
+    AVERAGE = "average"
+    PERCEPTUAL = "perceptual"
+    DIFFERENCE = "difference"
+    WAVELET = "wavelet"
+    CROP_RESISTANT = "crop_resistant"
+
+
+class ImageHasher:
+    def __init__(
+        self, hashing_algorithm: HashingAlgorithm, hash_size: Optional[int] = None
+    ):
+        self.hashing_algorithm = hashing_algorithm
+        self.hash_size = hash_size
+
+    def __call__(self, image: bytes | Image.Image) -> str:
+        if isinstance(image, bytes):
+            image = Image.open(io.BytesIO(image))
+        return str(self.hash_image(image))
+
+    def hash_image(self, image: Image.Image):
+        """
+        Args:
+            hashing_algorithm: The selected image hashing algorithm to use
+            hash_size: Size of the image hash
+
+        Returns:
+            Selected Image Hasher
+        """
+        kwargs = {}
+        if self.hash_size is not None:
+            kwargs["hash_size"] = self.hash_size
+
+        match self.hashing_algorithm:
+            case HashingAlgorithm.AVERAGE:
+                return imagehash.average_hash(image, **kwargs)
+            case HashingAlgorithm.PERCEPTUAL:
+                return imagehash.phash(image, **kwargs)
+            case HashingAlgorithm.DIFFERENCE:
+                return imagehash.dhash(image, **kwargs)
+            case HashingAlgorithm.WAVELET:
+                return imagehash.whash(image, **kwargs)
+            case HashingAlgorithm.CROP_RESISTANT:
+                return imagehash.crop_resistant_hash(image)
+            case _:
+                raise ValueError(
+                    f"Please select a valide hashing algorithm: {self.hashing_algorithm}"
+                )
