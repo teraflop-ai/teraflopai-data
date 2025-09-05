40366b21a1a134f4dde56ad2fe4d70c1181eb166
Enrico Shippole <enricoship@gmail.com>
2025-09-05 00:46:27 -0400

    Falcons NSFW classifier

--- src/teraflopai_data/components/image/nsfw.py
+++ src/teraflopai_data/components/image/nsfw.py
@@ -3,11 +3,11 @@ from typing import Optional
 import daft
 import torch
 from daft import DataType
-
+from PIL import Image
 from src.teraflopai_data.components.distributed_base import Distributed
 
 
-def create_finewebedu_udf(
+def create_falcon_nsfw_udf(
     model_name: str,
     batch_size: Optional[int] = None,
     concurrency: Optional[int] = None,
@@ -15,62 +15,47 @@ def create_finewebedu_udf(
     num_gpus: Optional[int] = None,
 ):
     @daft.udf(
-        return_dtype=DataType.int8(),
+        return_dtype=DataType.string(),
         concurrency=concurrency,
         num_cpus=num_cpus,
         num_gpus=num_gpus,
         batch_size=batch_size,
     )
-    class FinewebEduUDF:
+    class FalconsNSFWUDF:
         def __init__(
             self,
             model_name: str = model_name,
             device: str = "cuda",
-            torch_dtype: torch.dtype = torch.bfloat16,
-            attn_implementation: str = "sdpa",
         ):
-            from transformers import AutoModelForSequenceClassification, AutoTokenizer
-
+            from transformers import AutoImageProcessor, AutoModelForImageClassification
+            
             self.device = device
 
-            self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-            self.model = AutoModelForSequenceClassification.from_pretrained(
-                model_name,
-                torch_dtype=torch_dtype,
-                attn_implementation=attn_implementation,
-            ).to(self.device)
+            self.processor = AutoImageProcessor.from_pretrained(model_name)
+            self.model = AutoModelForImageClassification.from_pretrained(model_name).to(self.device)
             self.model.compile()
             self.model.eval()
 
-        def __call__(self, text_col: daft.DataFrame) -> daft.DataFrame:
-            inputs = self.tokenizer(
-                text_col.to_pylist(),
-                return_tensors="pt",
-                padding="longest",
-                truncation=True,
-            ).to(self.device)
-
+        def __call__(self, images: daft.DataFrame) -> daft.DataFrame:
+            inputs = self.processor(images=[Image.fromarray(img) for img in images], return_tensors="pt").to(self.device)
             with torch.no_grad():
-                outputs = self.model(**inputs)
-                logits = outputs.logits.squeeze(-1).float().cpu().numpy()
-
-            scores = [int(round(max(0, min(score, 5)))) for score in logits]
-
+                outputs = self.model(**inputs).logits
+            predicted_labels = outputs.argmax(-1)
+            scores = [self.model.config.id2label[p.cpu().item()] for p in predicted_labels]
             return daft.Series.from_pylist(scores)
 
-    return FinewebEduUDF.with_init_args(
+    return FalconsNSFWUDF.with_init_args(
         model_name=model_name,
     )
 
 
-class FinewebEduClassifier(Distributed):
+class FalconsNSFWClassifier(Distributed):
     def __init__(
         self,
-        model_name: str = "HuggingFaceTB/fineweb-edu-classifier",
+        model_name: str = "Falconsai/nsfw_image_detection",
         batch_size: int = 1,
         input_column: str = None,
-        output_column: Optional[str] = "finewebedu_score",
+        output_column: Optional[str] = "falconsnsfw_score",
         concurrency: Optional[int] = None,
         num_cpus: Optional[int] = None,
         num_gpus: Optional[int] = None,
@@ -86,7 +71,7 @@ class FinewebEduClassifier(Distributed):
         )
 
     def _udf(self):
-        return create_finewebedu_udf(
+        return create_falcon_nsfw_udf(
             model_name=self.model_name,
             batch_size=self.batch_size,
             concurrency=self.concurrency,
